9d18969975ac0d23f8469c50b7af4175c64446dd
Igor Popov <gaurnar@gmail.com>
2018-02-12 09:53:02 +0700

    minor colors fix

--- src/org/gsoft/showcase/diff/gui/Colors.java
+++ src/org/gsoft/showcase/diff/gui/Colors.java
@@ -6,7 +6,7 @@ public final class Colors {
     public static final Color DELETED_LINES_HIGHLIGHT_COLOR = new Color(250, 180, 170);
     public static final Color INSERTED_LINES_HIGHLIGHT_COLOR = new Color(174, 255, 202);
     public static final Color MODIFIED_LINES_HIGHLIGHT_COLOR = new Color(221, 226, 255);
-    public static final Color MODIFIED_CHARS_HIGHLIGHT_COLOR = new Color(187, 211, 255);
+    public static final Color MODIFIED_CHARS_HIGHLIGHT_COLOR = new Color(168, 191, 234);
 
     private Colors() {
         throw new UnsupportedOperationException();
